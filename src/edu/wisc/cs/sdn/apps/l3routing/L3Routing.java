c296127b0f2113a2e66aaea8d05e377a092077f8
ZichuanTian <zctian@yahoo.com>
2018-11-21 16:08:35 -0600

    changed to map(map). Fuck Java no tuple.

--- src/edu/wisc/cs/sdn/apps/l3routing/L3Routing.java
+++ src/edu/wisc/cs/sdn/apps/l3routing/L3Routing.java
@@ -88,7 +88,8 @@ public class L3Routing implements IFloodlightModule, IOFSwitchListener,
 		
 		/*********************************************************************/
 		/* TODO: Initialize variables or perform startup tasks, if necessary */
-		
+		this.updateShortestPath();
+		this.installRules();
 		/*********************************************************************/
 	}
 
